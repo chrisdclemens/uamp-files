196975d23fb1dc5da6156cb946208b3c9c4950c8
Chris Lee <chris@Levelten-CL.(none)>
2012-08-27 12:39:03 -0500

    Update setup for setting up pma; added patch

--- phpmyadmin/setup-apache.sh
+++ phpmyadmin/setup-apache.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 export PATH_A2=/etc/apache2/apache2.conf
-
-echo $PATH_A2
-#echo "Include /etc/phpmyadmin/apache.conf" | sudo tee 
+echo "Include /etc/phpmyadmin/apache.conf" | sudo tee -a /etc/apache2/apache2.conf
---
+++ phpmyadmin/setup-pma-config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+sudo cp config.inc.php /etc/phpmyadmin/config.inc.php
