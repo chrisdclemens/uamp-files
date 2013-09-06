dbc253c3a45ac5a5881ac71b8f78ab97400de5d4
Chris J. Lee <iam@chrisjlee.net>
2013-09-06 18:17:41 -0500

    Update setup.sh
    Update git url of drush

--- drush/setup.sh
+++ drush/setup.sh
@@ -4,7 +4,7 @@
 # http://drupalcode.org/project/drush.git/blob_plain/HEAD:/README.txt
 #
 echo "Cloning Drush into $HOME"
-git clone --recursive --branch 7.x-4.x http://git.drupal.org/project/drush.git $HOME/drush > /dev/null
+git clone --recursive --branch 7.x-4.x https://github.com/drush-ops/drush.git $HOME/drush > /dev/null
 sudo mv $HOME/drush /etc/drush
 sudo chmod u+x /etc/drush/drush.php
 echo "Changing Permissions of '/etc/drush/drush.php'"
